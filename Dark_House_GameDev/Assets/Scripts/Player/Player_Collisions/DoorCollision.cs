261b4285ad2d02e43711cb4771e2ffdbe41b2f4c
Lucas Duez <lucas.verzaro@gmail.com>
2021-12-21 10:36:27 -0300

    Chaves e portas implementadas

---
+++ Dark_House_GameDev/Assets/Scripts/Player/Player_Collisions/DoorCollision.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace Player.Player_Collisions{
+    public class DoorCollision : BaseCollision{
+
+        private int _numberOfKeysNeeded;
+
+        private void Start() {
+            var keysGameObjsArray = GameObject.FindGameObjectsWithTag("Key");
+
+            _numberOfKeysNeeded = keysGameObjsArray.Length;
+            
+        }
+
+        protected override void defaultMethod(Collision2D other) {
+            if (PlayerKeyInventory.KeyCount >= _numberOfKeysNeeded) {
+                print(PlayerKeyInventory.KeyCount);
+                print("Abre-te sésamo");
+            }
+            
+        }
+    }
+}
---
+++ Dark_House_GameDev/Assets/Scripts/Player/Player_Collisions/PlayerKeyCollision.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace Player.Player_Collisions{
+    public class PlayerKeyCollision : BaseCollision{
+        protected override void defaultMethod(Collision2D other) {
+            PlayerKeyInventory.AddOneKey();
+            Destroy(other.gameObject);
+        }
+
+        protected override void OnDisable() {
+            var numberOfKeysEnable = GameObject.FindGameObjectsWithTag(tag).Length;
+
+            if (numberOfKeysEnable <= 1) {
+                base.OnDisable();
+            }
+        }
+    }
+}
