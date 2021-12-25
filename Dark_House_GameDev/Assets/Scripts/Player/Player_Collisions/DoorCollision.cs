b4fcac997b2715df2713367b698902966a437fc6
Lucas Duez <lucas.verzaro@gmail.com>
2021-12-25 12:26:16 -0300

    merge

--- Dark_House_GameDev/Assets/Scripts/Player/Player_Collisions/DoorCollision.cs
+++ Dark_House_GameDev/Assets/Scripts/Player/Player_Collisions/DoorCollision.cs
@@ -5,16 +5,31 @@ namespace Player.Player_Collisions{
 
         private int _numberOfKeysNeeded;
 
+        private AudioSource _audioSource;
+
+        [SerializeField]private AudioClip[] _audioClipsArray;
+
         private void Start() {
             var keysGameObjsArray = GameObject.FindGameObjectsWithTag("Key");
 
             _numberOfKeysNeeded = keysGameObjsArray.Length;
+
+            _audioSource = GetComponent<AudioSource>();
             
+            print("Precisam de "+_numberOfKeysNeeded);
+
         }
 
         protected override void defaultMethod(Collision2D other) {
             if (PlayerKeyInventory.KeyCount >= _numberOfKeysNeeded) {
+                _audioSource.clip = _audioClipsArray[0];
+                _audioSource.Play();
                 print("Abre-te sésamo");
+                
+            }
+            else if(!_audioSource.isPlaying) {
+                _audioSource.clip = _audioClipsArray[1];
+                _audioSource.Play();
             }
             
         }
