d46b4af7dfb34b4b2ecb27a2b5f73d2a0b688560
Lucas Duez <lucas.verzaro@gmail.com>
2021-12-28 12:43:31 -0300

    Arrumei a fase e deixei um pouco mais jogável

--- Dark_House_GameDev/Assets/Scripts/Game_Scripts/Player/Player_Collisions/PlayerCollisionManager.cs
+++ Dark_House_GameDev/Assets/Scripts/Game_Scripts/Player/Player_Collisions/PlayerCollisionManager.cs
@@ -6,7 +6,7 @@ namespace Player.Player_Collisions{
     public class PlayerCollisionManager : MonoBehaviour{
         private static Dictionary<string, Action<Collision2D>> _collisionDictionary = new Dictionary<string, Action<Collision2D>>();
         
-        private void OnCollisionEnter2D(Collision2D other) {
+        private void OnCollisionStay2D(Collision2D other) {
             string gameTag = other.gameObject.tag;
 
             if (_collisionDictionary.ContainsKey(gameTag)) _collisionDictionary[gameTag]?.Invoke(other);
