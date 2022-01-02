de3366917ef309e3e71fd092457d185bf6735836
Lucas Duez <lucas.verzaro@gmail.com>
2022-01-02 19:26:54 -0300

    Implementei os novos sons das chaves

--- Dark_House_GameDev/Assets/Scripts/Game_Scripts/Player/Player_Collisions/PlayerKeyCollision.cs
+++ Dark_House_GameDev/Assets/Scripts/Game_Scripts/Player/Player_Collisions/PlayerKeyCollision.cs
@@ -5,6 +5,8 @@ namespace Player.Player_Collisions{
         private AudioSource _audioSource;
         private Collider2D _collider2D;
         private SpriteRenderer _spriteRenderer;
+
+        [SerializeField] private AudioClip[] _audioClipsArray;
         
         private void Start() {
             _audioSource = GetComponent<AudioSource>();
@@ -13,22 +15,33 @@ namespace Player.Player_Collisions{
             position = new Vector3(position.x, position.y, 0f);
             
             transform.position = position;
+
+            _audioSource.clip = _audioClipsArray[0];
+            _audioSource.Play();
         }
 
         protected override void defaultMethod(Collision2D other) {
             PlayerKeyInventory.AddOneKey();
+            ChangeAudioWhenCollide(other);
+            DeactivateColliderAndSpriteRenderer(other);
 
+            print($"Nós temos {PlayerKeyInventory.KeyCount} chaves");
+        }
+
+        private void DeactivateColliderAndSpriteRenderer(Collision2D other) {
             _collider2D = other.gameObject.GetComponent<Collider2D>();
             _spriteRenderer = other.gameObject.GetComponent<SpriteRenderer>();
-           
-            if (gameObject.activeInHierarchy) {
-                _audioSource.Play();
-            }
-
             _collider2D.enabled = false;
             _spriteRenderer.enabled = false;
-            
-            print($"Nós temos {PlayerKeyInventory.KeyCount} chaves");
+        }
+
+        private void ChangeAudioWhenCollide(Collision2D other) {
+            var audioSource = other.gameObject.GetComponent<AudioSource>();
+            audioSource.Stop();
+            audioSource.loop = false;
+            audioSource.clip = _audioClipsArray[1];
+            audioSource.spatialBlend = 0f;
+            audioSource.Play();
         }
 
         protected override void OnDisable() {
