e8be23adc4cedacc2a23737d5ef40d6ae592158c
Lucas Duez <lucas.verzaro@gmail.com>
2022-01-02 00:28:58 -0300

    Tentei balancear o jogo

--- Dark_House_GameDev/Assets/Scripts/Game_Scripts/YouLoseEvent.cs
+++ Dark_House_GameDev/Assets/Scripts/Game_Scripts/YouLoseEvent.cs
@@ -1,17 +1,32 @@
 ﻿using Game_Scripts.Monster;
 using Game_Scripts.Monster.State_Machine;
+using Player;
 using UnityEngine;
 
 namespace DefaultNamespace{
     public class YouLoseEvent : MonoBehaviour{
+        [SerializeField] private AudioClip _audio;
+        
         private void Start() {
             var monsterStatemachine = MonsterSingleton.Instance.GetComponent<MonsterStateMachineManager>();
 
             monsterStatemachine.WalkingNearbyPlayerState.OnGameOver += kappa;
+            monsterStatemachine.WalkingNearbyPlayerState.OnGameOver += otherKappa;
         }
 
         public void kappa() {
-            print("O evento de game over foi chamado!");
+            var a = PlayerSingleton.Instance.GetComponent<AudioSource>();
+            a.clip = _audio;
+            a.Play();
+        }
+
+
+        public void otherKappa() {
+            var a = GameObject.FindGameObjectsWithTag("Guide");
+
+            foreach (var item in a) {
+                item.SetActive(false);
+            }
         }
     }
 }
