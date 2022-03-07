6303cf4cc0f94530fa6249b3c10a96ca53b5a2c4
Lucas Duez <46378322+Lucasdvs10@users.noreply.github.com>
2022-03-07 16:38:19 -0300

    '

--- Assets/Scripts/Game_Scripts/Player/State_Machine/WalkingPlayerState.cs
+++ Assets/Scripts/Game_Scripts/Player/State_Machine/WalkingPlayerState.cs
@@ -28,7 +28,7 @@ namespace Player.State_Machine{
             UpdateAudioClipOnAudioSource();
             
             if(!_audioSource.isPlaying)
-                _audioSource.PlayDelayed(0.3f);
+                _audioSource.PlayDelayed(0.54f);
             
             if (_playerRigidbody.velocity.magnitude <= _playerSingleton.MovingVelocity * 0.20f) {
                 PlayerStateMachine.ChangeCurrentState(PlayerStateMachine.IdlePlayerState);
