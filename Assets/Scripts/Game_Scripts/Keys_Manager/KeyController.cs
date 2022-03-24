d7cc0ab43f2237902b1262f80308a45d6137d6bd
Felipe Rodrigues <frps2003@gmail.com>
2022-03-23 22:29:14 -0300

    refatoração no sistema de coleta de chave/tecla

--- Assets/Scripts/Game_Scripts/Keys_Manager/KeyController.cs
+++ Assets/Scripts/Game_Scripts/Keys_Manager/KeyController.cs
@@ -8,6 +8,7 @@ namespace Game_Scripts.Keys_Manager
     {
         public event Action OnCollectingEvent;
         public event Action OnActivatingEvent;
+        public event Action OnDeActivatingEvent;
         
         private bool _currentActive;
         private bool _collected;
@@ -20,6 +21,15 @@ namespace Game_Scripts.Keys_Manager
             }
             _currentActive = true;
             OnActivatingEvent?.Invoke();
+        }
+
+        public void SetFalse()
+        {
+            _currentActive = false;
+            OnDeActivatingEvent?.Invoke();
+            // todo: fazer todos os gameobjs começarem ativos, e dps com essa função e a função de cima, setar tudo para os sons tocarem corretamente
+            // e quando tiver sido coletado tocar um som diferente
+            // quando desativado tocar outro som
         }
 
         public void Collect()
