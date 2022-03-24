0b7a65d7869280458c3af0213bc008595a86a74b
Lucas Duez <46378322+Lucasdvs10@users.noreply.github.com>
2022-03-24 10:46:33 -0300

    Tottens implementados ja com resposta sonora

--- Assets/Scripts/Game_Scripts/Second_Puzzle/PuzzleManager.cs
+++ Assets/Scripts/Game_Scripts/Second_Puzzle/PuzzleManager.cs
@@ -6,25 +6,30 @@ using UnityEngine;
 namespace DefaultNamespace.Second_Puzzle{
     public class PuzzleManager : MonoBehaviour{
         private int _currentIndex;
-        public static event Action SucessfulInteractingEvent;
+        public static event Action SucessfullInteractingEvent;
         public static event Action FailedInteractingEvent;
         
-        [SerializeField] private List<string> _objsTagsList;
+        [SerializeField] private List<GameObject> _gameObjectsList;
 
         private void Awake() {
             FailedInteractingEvent += ResetCurrentIndex;
-            SucessfulInteractingEvent += NextIndex;
+            SucessfullInteractingEvent += NextIndex;
         }
 
-        public void CheckIfInputTagIsRight(string tag) {
-            if (tag == _objsTagsList[_currentIndex])
-                SucessfulInteractingEvent?.Invoke();
+        private void OnDestroy() {
+            FailedInteractingEvent -= ResetCurrentIndex;
+            SucessfullInteractingEvent -= NextIndex;
+        }
+
+        public void CheckIfInputObjIsRight(GameObject gameObject) {
+            if (gameObject == _gameObjectsList[_currentIndex])
+                SucessfullInteractingEvent?.Invoke();
             else
                 FailedInteractingEvent?.Invoke();
         }
 
         private void NextIndex() {
-            if (_currentIndex == _objsTagsList.Count - 1) { //se chamar a função e estiver no funial da lista, quer dizer que a senha está certa. Fim de jogo
+            if (_currentIndex == _gameObjectsList.Count - 1) { //se chamar a função e estiver no final da lista, quer dizer que a senha está certa. Fim de jogo
                 EventManager.InvokeOnGameWin();
                 return;
             }
@@ -34,6 +39,9 @@ namespace DefaultNamespace.Second_Puzzle{
         private void ResetCurrentIndex() {
             _currentIndex = 0;
         }
-        
+
+        public int CurrentIndex => _currentIndex;
+
+        public int ListLength => _gameObjectsList.Count;
     }
 }
---
+++ Assets/Scripts/Game_Scripts/Second_Puzzle/Radio.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+namespace DefaultNamespace.Second_Puzzle{
+    [RequireComponent(typeof(AudioSource))]
+    public class Radio : MonoBehaviour{
+        [SerializeField] private AudioClip[] _audioClipsArray;
+        [SerializeField] private PuzzleManager _puzzleManager;
+
+        private AudioSource _audioSource;
+        private void Awake() {
+            _audioSource = GetComponent<AudioSource>();
+            
+            _audioSource.clip = _audioClipsArray[0];
+            _audioSource.Play();
+
+        }
+
+        private void Start() {
+            PuzzleManager.FailedInteractingEvent += UpdateCurrentClipAndPlay;
+            PuzzleManager.SucessfullInteractingEvent += UpdateCurrentClipAndPlay;
+        }
+
+        private void OnDestroy() {
+            PuzzleManager.FailedInteractingEvent -= UpdateCurrentClipAndPlay;
+            PuzzleManager.SucessfullInteractingEvent -= UpdateCurrentClipAndPlay;
+        }
+
+        private void UpdateCurrentClipAndPlay() {
+            var i = _puzzleManager.CurrentIndex;
+            _audioSource.clip = _audioClipsArray[i];
+            
+            _audioSource.Play();
+        }
+    }
+}
